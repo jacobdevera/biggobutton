99a091dd321ff750842c08b2cfe62515a61a91c2
Jacob Devera <jacob.devera12@gmail.com>
2017-01-22 20:55:01 -0800

    Created button and event handler

--- app/src/main/java/edu/washington/gjdevera/biggobutton/MainActivity.java
+++ app/src/main/java/edu/washington/gjdevera/biggobutton/MainActivity.java
@@ -2,6 +2,8 @@ package edu.washington.gjdevera.biggobutton;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btnMain);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
     }
 }
